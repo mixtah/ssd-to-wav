970666326b0aa3a5a3d2c350d426c78df184b283
Michael Bauer <da1da2da3ofme@gmail.com>
2016-04-20 18:03:30 +1000

    Added usage and 2 checking functions + start initial logic

--- ssdwav.py
+++ ssdwav.py
@@ -1,12 +1,35 @@
 import sys,os,wave,time
 
 def usage():
-	print "ssdwav.py <Input file or directory> <Output file or directory>"
-
+    print "Converts SSD format file into WAV files. Will convert an individual file or entire directory"
+	print "Usage: ssdwav.py <'-r'?> <'-o'?> <Input file or directory> <Directory>"
+    print "-r : Process directories recursively and convert all ssd files"
+    print "-o : Override .ssd files with produced .wav files (Doesn't require output directory)"
+   
 def isSSDFile(file):
-	if file.split('.',1)[1] == 'ssd':
+    #check if file ends in .ssd
+	if file.split('.')[-1] == 'ssd':
 		return True
 	else:
 		return False
 
-if len(sys.argv) != 2
+#Used to decide between converting a directory full of files or a single file
+def isDirectory(directory):
+    #check if last part in directory doesn't have a file extension
+    if len(directory.split(os.path.sep)[-1].split('.'))==1:
+        return True
+    else:
+        return False
+
+if len(sys.argv) == 3:
+    #do file conversion or directory conversion
+    #or file conversion with override or directory conversion with override
+    
+elif len(sys.argv) == 4:
+    #do recursive directory conversion or override directory conversion
+    
+else:
+    usage()
+    exit()
+
+    
